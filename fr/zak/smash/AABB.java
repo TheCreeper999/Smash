5843656361f5886486da27075aa6fbaab9e7ae24
TheCreeper999 <alex.ulysse@gmail.com>
2014-02-07 19:27:12 +0100

    Smash

---
+++ fr/zak/smash/AABB.java
@@ -0,0 +1,13 @@
+package fr.zak.smash;
+
+public class AABB {
+
+	int x, y, w, h;
+	
+	public AABB(int x, int y, int w, int h){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+}
--- fr/zak/smash/Launch.java
+++ fr/zak/smash/Launch.java
@@ -1,6 +1,8 @@
 package fr.zak.smash;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -16,6 +18,9 @@ public class Launch {
 	public JoueurMP jmp = new JoueurMP();
 	
 	private static Launch instance = new Launch();
+	
+	boolean game = false;
+	boolean button1 = false;
 
 	/** time at last frame */
 	long lastFrame;
@@ -46,7 +51,7 @@ public class Launch {
 			int delta = getDelta();
  
 			update(delta);
-			new Thread(new ThreadClient(j.getDirection(), j.getX(), j.getY())).start();
+//			new Thread(new ThreadClient(j.getDirection(), j.getX(), j.getY())).start();
 			renderGL();
  
 			Display.update();
@@ -59,8 +64,23 @@ public class Launch {
 	int level = 0;
 	public void update(int delta) {
 		//if (Keyboard.isKeyPressed(...
-		j.update();
-		tirs.update(j.getX(), j.getY());
+		if(game){
+			j.update();
+			tirs.update(j.getX(), j.getY());
+			if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)){
+				game = false;
+			}
+		}
+		if(!game){
+			AABB button1aabb = new AABB(Display.getWidth() / 2 - 100, Display.getHeight() / 2 + 40, 200, 13);
+			if(Mouse.isButtonDown(0) && collisionMouse(Mouse.getX(), Mouse.getY(), button1aabb)){
+				button1 = true;
+				game = true;
+			}
+			else{
+				button1 = false;
+			}
+		}
 		updateFPS();
 	}
  
@@ -109,43 +129,67 @@ public class Launch {
 	public void renderGL() {
 		GL11.glPushMatrix();
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glColor3f(0.2F, 0.6F, 0F);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(0, 0);
-		GL11.glVertex2f(0, 100);
-		GL11.glVertex2f(800, 100);
-		GL11.glVertex2f(800, 0);
-		GL11.glEnd();
-		GL11.glColor3f(0, 0.4F, 1);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(0, 100);
-		GL11.glVertex2f(0, 600);
-		GL11.glVertex2f(800, 600);
-		GL11.glVertex2f(800, 100);
-		GL11.glEnd();
-		GL11.glColor3f(1F, 0.7F, 0);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(700, 500);
-		GL11.glVertex2f(700, 560);
-		GL11.glVertex2f(760, 560);
-		GL11.glVertex2f(760, 500);
-		GL11.glEnd();
-		GL11.glColor3f(1F, 1F, 0);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(705, 505);
-		GL11.glVertex2f(705, 555);
-		GL11.glVertex2f(755, 555);
-		GL11.glVertex2f(755, 505);
-		GL11.glEnd();
-		GL11.glColor3f(1F, 1F, 0f);
-		GL11.glBegin(GL11.GL_QUADS);
-		tirs.gen();
-		GL11.glEnd();
-		j.gen(tirs.getDirection());
-		jmp.gen(Reception.direction, Reception.x, Reception.y);
+		if(game){
+			GL11.glColor3f(0.2F, 0.6F, 0F);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(0, 0);
+			GL11.glVertex2f(0, 100);
+			GL11.glVertex2f(800, 100);
+			GL11.glVertex2f(800, 0);
+			GL11.glEnd();
+			GL11.glColor3f(0, 0.4F, 1);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(0, 100);
+			GL11.glVertex2f(0, 600);
+			GL11.glVertex2f(800, 600);
+			GL11.glVertex2f(800, 100);
+			GL11.glEnd();
+			GL11.glColor3f(1F, 0.7F, 0);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(700, 500);
+			GL11.glVertex2f(700, 560);
+			GL11.glVertex2f(760, 560);
+			GL11.glVertex2f(760, 500);
+			GL11.glEnd();
+			GL11.glColor3f(1F, 1F, 0);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(705, 505);
+			GL11.glVertex2f(705, 555);
+			GL11.glVertex2f(755, 555);
+			GL11.glVertex2f(755, 505);
+			GL11.glEnd();
+			GL11.glColor3f(1F, 1F, 0f);
+			GL11.glBegin(GL11.GL_QUADS);
+			tirs.gen();
+			GL11.glEnd();
+			j.gen(tirs.getDirection());
+			jmp.gen(Reception.direction, Reception.x, Reception.y);
+		}
+		if(!game){
+			if(!button1){
+				GL11.glColor3f(1F, 0, 0);
+			}
+			else{
+				GL11.glColor3f(0.5F, 0, 0);
+			}
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(Display.getWidth() / 2 - 100, Display.getHeight() / 2 + 40);
+			GL11.glVertex2f(Display.getWidth() / 2 - 100, Display.getHeight() / 2 + 40 + 13);
+			GL11.glVertex2f(Display.getWidth() / 2 + 100, Display.getHeight() / 2 + 40 + 13);
+			GL11.glVertex2f(Display.getWidth() / 2 + 100, Display.getHeight() / 2 + 40);
+			GL11.glEnd();
+		}
 		GL11.glPopMatrix();
 	}
-
+	
+	private boolean collisionMouse(int mouseX, int mouseY, AABB box){
+		   if(mouseX >= box.x && mouseX <= box.x + box.w && mouseY >= box.y && mouseY <= box.y + box.h){
+			   return true;
+		   }
+		   else{
+			   return false;
+		   }
+	}
 	
 	public static void main(String[] argv) {
 		Launch launch = new Launch();
